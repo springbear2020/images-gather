fba395123fd8cf77e14313416ff8b3c6b69c36a1
leoli <leoli@yeahka.com>
2024-11-23 19:36:52 +0800

    deploy: changes for deployment in WSL -d Ubuntu-24.04

--- src/main/webapp/static/js/gather-commons.js
+++ src/main/webapp/static/js/gather-commons.js
@@ -3,7 +3,7 @@
  * Constants
  * =====================================================================================================================
  */
-var contextPath = "http://localhost:8080/";
+var contextPath = "/images-gather/";
 // Images' url
 var notChooseImageUrl = contextPath + "static/img/notChoose.png";
 var invalidImageUrl = contextPath + "static/img/invalid.png";
